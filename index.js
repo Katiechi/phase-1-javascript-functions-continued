a9b2c82c55ff203ff8a62744e747c5f90355b4e0
brian ambundo <brian.katiechi9@gmail.com>
2022-06-08 13:58:21 +0300

    commit tree d1986b122de7960f0afad68934b87e0e00390c87

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+function saturdayFun(activity){
+    if(typeof activity === "string"){
+        return `This Saturday, I want to ${activity}!`
+    }
+    else{
+        return `This Saturday, I want to roller-skate!`
+    }
+    
+}
+console.log(saturdayFun(" make music"))
+
+function mondayWork(parameter){
+    if(typeof parameter === 'string'){
+        return `This Monday, I will ${parameter}.`
+    }
+    else{
+        return "This Monday, I will go to the office."
+    }
+}
+console.log(mondayWork('work from home.'))
+
+
+function wrapAdjective(result){
+    const message = function (emphatic){
+        if(result === '*'){
+            return `You are ${result}${emphatic}${result}!`
+        }
+        else if(result === '||'){
+            return `You are ${result}${emphatic}${result}!`
+        }
+
+    }
+    return message
+}
+
+console.log(wrapAdjective("*")("a hard worker"))
